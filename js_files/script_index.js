c7af13354c6bed4717bcfc5c2347448090f1a56f
wakisa-chihana <wakisachihana422@gmail.com>
2024-11-18 10:14:24 -0700

    fetching new things

---
+++ js_files/script_index.js
@@ -0,0 +1,5 @@
+// Toggles the visibility of the sidebar and main content
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('mainContent').classList.toggle('active');
+}
